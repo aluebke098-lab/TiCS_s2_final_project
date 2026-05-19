fc144d0e3fe6a3dd1c22ce0910df2af2090cfed4
aluebke098-lab <aluebke098@phm.k12.in.us>
2026-05-19 09:51:07 -0400

    updating game
    adding more to tilemap, fixing emeny collisions

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-@onready var player_node = get_parent().get_node("Player")
-var speed = 100.0
+@onready var player_node = get_parent().get_parent().get_node("Player")
+var speed = 150.0
 var gravity = 15 
+var active = false
 
 @export_range(-1,1) var dir = -1
 
 func _ready() -> void:
+	print(player_node)
+	
 	if dir == 0:
 		dir = -1
 	
@@ -15,23 +18,35 @@ func _ready() -> void:
 	else:
 		$Sprite2D.flip_h = false
 
-func _physics_process(delta: float) -> void: #if I want them to be able to fall off, remove the floor rays
-	if dir == -1 and $left_wall_ray.is_colliding():
-		$Sprite2D.flip_h = true
-		dir = 1
-	if dir == 1 and $right_wall_ray.is_colliding():
-		$Sprite2D.flip_h = false
-		dir = -1
-	
-	velocity.x = lerp(velocity.x, dir * speed, 10.0*delta)
-	velocity.y += gravity #for flying enemies, would remove gravity (and bottom rect collision shape)
-	move_and_slide()
+func _physics_process(delta: float) -> void: 
+	if active:
+		if dir == -1 and $left_wall_ray.is_colliding():
+			$Sprite2D.flip_h = true
+			dir = 1
+		if dir == 1 and $right_wall_ray.is_colliding():
+			$Sprite2D.flip_h = false
+			dir = -1
+		
+		velocity.x = lerp(velocity.x, dir * speed, 10.0*delta)
+		velocity.y += gravity #for flying enemies, would remove gravity 
+		move_and_slide()
 
 #func _wait_dir_change(desired_dir):
 	#dir = 0
 	#await get_tree().create_timer(.5).timeout
 	#dir = desired_dir
+ 
 
-func _on_area_2d_body_entered(body: Node2D) -> void: #temp, will prob add a secondary collision for defeating enemy? or maybe an attack action for player
+func _on_body_collision_body_entered(body: Node2D) -> void:
 	if body == player_node:
 		get_tree().call_deferred("reload_current_scene")
+
+func _on_death_collision_body_entered(body: Node2D) -> void:
+	if body == player_node:
+		queue_free()
+
+func _on_screen_entered() -> void:
+	active = true
+
+func _on_screen_exited() -> void:
+	active = false
